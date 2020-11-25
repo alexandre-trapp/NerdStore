77eef3b1f9339b3408ef524a95138e101669a239
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-25 20:33:13 -0300

    Add project files.

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+
+        public Guid CategoriaId { get; private set; }
+        public Categoria Categoria { get; private set; }
+
+        public Produto(
+            string nome, 
+            string descricao, 
+            bool ativo, 
+            decimal valor, 
+            DateTime dataCadastro, 
+            string imagem, 
+            Guid categoriaId)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+            CategoriaId = categoriaId;
+        }
+
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria) 
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao) => Descricao = descricao;
+
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0)
+                quantidade *= -1;
+
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade) => QuantidadeEstoque >= quantidade;
+
+        public void Validar()
+        {
+
+        }
+    }
+
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString() => $"{Nome} - {Codigo}";
+    }
+}
