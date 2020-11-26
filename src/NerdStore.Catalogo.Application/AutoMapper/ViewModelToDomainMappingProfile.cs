0a2f5c6a3545abe273b56f92e57aa3b4bc8c5282
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-11-26 18:58:50 -0300

    add automapper

---
+++ src/NerdStore.Catalogo.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Domain.Entities;
+using NerdStore.Catalogo.Application.ViewModels;
+
+namespace NerdStore.Catalogo.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProdutoViewModel, Produto>()
+                .ConstructUsing(p => 
+                    new Produto(p.Nome, p.Descricao, p.Ativo,
+                        p.Valor, p.CategoriaId, p.DataCadastro, 
+                        p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NerdStore.Catalogo.Application.ViewModels;
+
+namespace NerdStore.Catalogo.Application.Services
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int categoriaId);
+        Task<IEnumerable<CategoriaViewModel>> ObterCategorias();
+
+        Task AdicionarProduto(ProdutoViewModel produtoViewModel);
+        Task AtualizarProduto(ProdutoViewModel produtoViewModel);
+
+        Task<ProdutoViewModel> DebitarEstoque(Guid id, int quantidade);
+        Task<ProdutoViewModel> ReporEstoque(Guid id, int quantidade);
+    }
+}
